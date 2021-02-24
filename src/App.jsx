383cc6938fa8426bed9a266cd0e3e26fdd43fb51
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-24 13:24:11 -0500

    Adds basic HTML and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <div>
+      <header>
+        <h1>Currency Converter</h1>
+      </header>
+      <section>
+        <label htmlFor="Currency">Currency Amount: </label>
+        <input type="number" placeholder="1.00" name="Currency"></input>
+        <ul>
+          <li>All the Conversions</li>
+          <li>Mapped</li>
+        </ul>
+      </section>
+      <footer>This Currency Converter App was created by Mandy Wade</footer>
+    </div>
+  )
 }
